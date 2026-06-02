c7fcdfb1ad25b14c21dd0eb607048f954d29129a
Divya Pal <paldivya595@gmail.com>
2026-06-02 12:39:37 +0530

    Add total contact count on dashboard

--- public/Contact Book/app.py
+++ public/Contact Book/app.py
@@ -1,4 +1,4 @@
- flask import Flask, render_template, redirect, url_for, request, flash, session
+ from flask import Flask, render_template, redirect, url_for, request, flash, session
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 
@@ -9,7 +9,7 @@ db = SQLAlchemy(app)
 
 
 class User(db.Model):
-    id = db.Colufrommn(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(150), nullable=False)
 
@@ -65,10 +65,14 @@ def dashboard():
     user_id = session["user_id"]
     sort_order = request.args.get("sort", "asc")
 
-if sort_order == "desc":
-    contacts = Contact.query.filter_by(user_id=user_id).order_by(Contact.name.desc()).all()
-else:
-    contacts = Contact.query.filter_by(user_id=user_id).order_by(Contact.name.asc()).all()
+    if sort_order == "desc":
+        contacts = Contact.query.filter_by(user_id=user_id).order_by(
+            Contact.name.desc()
+        ).all()
+    else:
+        contacts = Contact.query.filter_by(user_id=user_id).order_by(
+            Contact.name.asc()
+        ).all()
 
     contact_count = len(contacts)
 
